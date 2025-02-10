4e00be3137c9ff8919e87e3eb755ff6e3594c187
hamawebdev <148373753+hamawebdev@users.noreply.github.com>
2025-02-10 23:13:15 +0100

    added middleware error handling

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
-
+// Initialize express
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+// Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+// Error handling middleware
+app.use(errorMiddleware);
 
+// Default route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Connect to database
 async function connectToDataBase() {
     await connectDB();
 }
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   connectToDataBase();
